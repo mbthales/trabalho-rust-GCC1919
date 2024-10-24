c7512a6feb997b9bcaaf0fed5ee25a1d2767c54b
Thales M <mbthales10@gmail.com>
2024-10-24 20:16:36 -0300

    Improvements in csv writer and reader functions

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,32 @@
 use csv::Reader;
 use csv::WriterBuilder;
 use std::fs::OpenOptions;
-use std::error::Error;
 
 fn main() {
-    if let Err(e) = csv_writer() {
-        println!("Error writing file: {}", e);
-    }
-
-    csv_reader();
+    let file_name = "bd.csv";
+    
+    csv_writer(file_name);
+    csv_reader(file_name);
 }
 
-fn csv_writer() -> Result<(), Box<dyn Error>> {
-    let file_name = "bd.csv";
+fn csv_writer(file_name: &str) {
     let file = OpenOptions::new()
         .append(true)
         .create(true)
-        .open(file_name)?;
+        .open(file_name);
 
+    if file.is_err() {
+        println!("Error reading file");
+        return;
+    }
+
+    let file = file.unwrap();
     let mut writer = WriterBuilder::new().from_writer(file);
 
-    writer.write_record(&["pergunta", "votos"])?;
-    writer.write_record(&["voce gosta de rust?", "90"])?;
-    writer.write_record(&["a", "b"])?;
-
-    Ok(())
+    let _ = writer.write_record(&["teste", "teste"]);
 }
 
-fn csv_reader(){
-    let file_name = "bd.csv";
+fn csv_reader(file_name: &str){
     let reader = Reader::from_path(file_name);
 
     if reader.is_err() {
