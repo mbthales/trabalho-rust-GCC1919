b20b50af791349aba411be0887cc8f42f6239696
IssamuTatch <issamu.oba.matsumoto@gmail.com>
2024-12-01 12:00:42 -0300

    Adding error cases for create poll

--- src/main.rs
+++ src/main.rs
@@ -64,6 +64,12 @@ impl fmt::Display for ValidationError {
     }
 }
 
+impl PartialEq for ValidationError {
+    fn eq(&self, other: &Self) -> bool {
+        self.details == other.details
+    }
+}
+
 impl Error for ValidationError {}
 
 impl FromSql for PollDuration {
@@ -468,7 +474,7 @@ impl Poll {
         } else{
             println!("\nQuestion can't be empty");
             return Err(Box::new(ValidationError::new(
-                "Question can't be empty",
+                "Question can't be empty.",
             )));
         }
         
@@ -901,7 +907,7 @@ mod tests {
     use std::fs;
     use std::path::Path;
 
-    use crate::io::Cursor;
+    use crate::ValidationError;
     
     #[test]
     fn test_get_polls() -> Result<()> {
@@ -914,26 +920,51 @@ mod tests {
     
         let now = Local::now().timestamp();
 
-        let poll = Poll {
+        let poll1 = Poll {
             id: Uuid::new_v4(),
-            question: "teste question?".trim().to_string(),
-            poll_duration: PollDuration::OneMonth,
-            create_date: Local::now().timestamp(),
-            expiration_date : Local::now().timestamp() + 24*60*60*30,
+            question: "Do you like Rust?".trim().to_string(),
+            poll_duration: PollDuration::OneWeek,
+            create_date: now,
+            expiration_date : now + 24*60*60*7,
             positive_votes: 0,
             negative_votes: 0,
         };
+
+        let poll2 = Poll {
+            id: Uuid::new_v4(),
+            question: "Do you like Python?".trim().to_string(),
+            poll_duration: PollDuration::OneMonth,
+            create_date: now,
+            expiration_date : now + 24*60*60*30,
+            positive_votes: 0,
+            negative_votes: 0,
+        };
+
+        let expected_polls = vec![&poll1, &poll2];
     
         conn.execute(
             "INSERT INTO Poll (id, question, poll_duration, create_date, expiration_date, positive_votes, negative_votes ) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7)",
             [
-                &poll.id.to_string(),
-                &poll.question,
-                &poll.poll_duration.to_string(),
-                &poll.create_date.to_string(),
-                &poll.expiration_date.to_string(),
-                &poll.positive_votes.to_string(),
-                &poll.negative_votes.to_string(),
+                &poll1.id.to_string(),
+                &poll1.question,
+                &poll1.poll_duration.to_string(),
+                &poll1.create_date.to_string(),
+                &poll1.expiration_date.to_string(),
+                &poll1.positive_votes.to_string(),
+                &poll1.negative_votes.to_string(),
+            ],
+        )?;
+
+        conn.execute(
+            "INSERT INTO Poll (id, question, poll_duration, create_date, expiration_date, positive_votes, negative_votes ) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7)",
+            [
+                &poll2.id.to_string(),
+                &poll2.question,
+                &poll2.poll_duration.to_string(),
+                &poll2.create_date.to_string(),
+                &poll2.expiration_date.to_string(),
+                &poll2.positive_votes.to_string(),
+                &poll2.negative_votes.to_string(),
             ],
         )?;
 
@@ -943,10 +974,9 @@ mod tests {
 
         match polls_output {
             Ok(polls) => {
-                let response = &polls[0];
-                println!("{:?}", response);
-                println!("{:?}", poll);
-                assert_eq!(response, &poll)
+                for (i, response) in polls.iter().enumerate() {
+                    assert_eq!(&response, &expected_polls[i], "Output was different than expected.");
+                }
             }
             Err(err) => {
                 panic!("Failed to retrieve polls: {:?}", err);
@@ -956,7 +986,47 @@ mod tests {
     }
 
     #[test]
-    fn test_create_poll() -> Result<()> {
+    fn test_create_poll_30days_duration() -> Result<()> {
+        println!("Starting Test");
+        let conn = Connection::open_in_memory()?;
+    
+        create_tables(&conn)?;
+    
+        let now = Local::now().timestamp();
+
+        let poll = Poll {
+            id: Uuid::new_v4(),
+            question: "Do You like Rust?".trim().to_string(),
+            poll_duration: PollDuration::OneMonth,
+            create_date: now,
+            expiration_date : now + 24*60*60*30,
+            positive_votes: 0,
+            negative_votes: 0,
+        };
+        
+        let poll_output = Poll::create_poll(&conn, "Do You like Rust?".to_string(), "30".to_string());
+
+        println!("{:?}", poll);
+        println!("{:?}", poll_output);
+
+        match poll_output {
+            Ok(poll_generated) => {
+                assert_eq!(poll.question, poll_generated.question);
+                assert_eq!(poll.poll_duration, poll_generated.poll_duration);
+                assert_eq!(poll.create_date, poll_generated.create_date);
+                assert_eq!(poll.expiration_date, poll_generated.expiration_date);
+                assert_eq!(poll.positive_votes, poll_generated.positive_votes);
+                assert_eq!(poll.negative_votes, poll_generated.negative_votes);
+            }
+            Err(e) => {
+                eprintln!("Error: {}", e);
+            }
+        }
+        Ok(())
+    }
+
+    #[test]
+    fn test_create_poll_7days_duration() -> Result<()> {
         println!("Starting Test");
         let conn = Connection::open_in_memory()?;
     
@@ -968,8 +1038,8 @@ mod tests {
             id: Uuid::new_v4(),
             question: "Do You like Rust?".trim().to_string(),
             poll_duration: PollDuration::OneWeek,
-            create_date: Local::now().timestamp(),
-            expiration_date : Local::now().timestamp() + 24*60*60*7,
+            create_date: now,
+            expiration_date : now + 24*60*60*7,
             positive_votes: 0,
             negative_votes: 0,
         };
@@ -993,5 +1063,93 @@ mod tests {
             }
         }
         Ok(())
+    }
+
+    #[test]
+    fn test_create_poll_too_long_question() -> Result<()> {
+        println!("Starting Test");
+        let conn = Connection::open_in_memory()?;
+    
+        create_tables(&conn)?;
+        
+        let poll_output = Poll::create_poll(&conn, "A".repeat(151).to_string(), "7".to_string());
+
+        let expected_error =  ValidationError::new(
+            "Question is too long. Question only can have up to 150 chars.",
+        );
+
+        match poll_output {
+            Err(err) => {
+                println!("Output error: {:?}", err);
+
+                // Tente converter o erro para ValidationError
+                if let Some(validation_error) = err.downcast_ref::<ValidationError>() {
+                    assert_eq!(validation_error, &expected_error, "Different Error Messages");
+                    Ok(())
+                } else {
+                    panic!("Expected ValidationError Type");
+                }
+            }
+            Ok(_) => panic!("Expected Error."),
+        }       
+    }
+
+    #[test]
+    fn test_create_poll_invalid_poll_duration() -> Result<()> {
+        println!("Starting Test");
+        let conn = Connection::open_in_memory()?;
+    
+        create_tables(&conn)?;
+        
+        let poll_output = Poll::create_poll(&conn, "Do You like Rust?".to_string(), "5".to_string());
+
+        let expected_error =  ValidationError::new(
+            "Invalid input_days value. Must be 7 or 30.",
+        );
+
+        match poll_output {
+            Err(err) => {
+                println!("Output error: {:?}", err);
+
+                // Tente converter o erro para ValidationError
+                if let Some(validation_error) = err.downcast_ref::<ValidationError>() {
+                    assert_eq!(validation_error, &expected_error, "Different Error Messages");
+                    Ok(())
+                } else {
+                    panic!("Expected ValidationError Type");
+                }
+            }
+            Ok(_) => panic!("Expected Error."),
+        }       
+    }
+
+    #[test]
+    fn test_create_poll_empty_question() -> Result<()> {
+        println!("Starting Test");
+        let conn = Connection::open_in_memory()?;
+    
+        create_tables(&conn)?;
+    
+        
+        let poll_output = Poll::create_poll(&conn, "".to_string(), "5".to_string());
+
+        let expected_error =  ValidationError::new(
+            "Question can't be empty.",
+        );
+
+        match poll_output {
+            Err(err) => {
+                println!("Output error: {:?}", err);
+
+                // Tente converter o erro para ValidationError
+                if let Some(validation_error) = err.downcast_ref::<ValidationError>() {
+                    assert_eq!(validation_error, &expected_error, "Different Error Messages");
+                    Ok(())
+                } else {
+                    panic!("Expected ValidationError Type");
+                }
+            }
+            Ok(_) => panic!("Expected Error."),
+        }       
     }
 }
